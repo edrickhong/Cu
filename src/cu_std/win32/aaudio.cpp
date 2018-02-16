c7b9139bd2b74adb7550fc08b39475213a05f33d
edrickhong <edrickhong@yahoo.com.sg>
2018-02-16 17:24:11 +0800

    fixed win32 compile bugs

--- src/cu_std/win32/aaudio.cpp
+++ src/cu_std/win32/aaudio.cpp
@@ -22,174 +22,165 @@ _persist VoiceCallback* voice_callback;
 #define _buffersize 880000
 //signed 16 bit range -32,768 to 32,767
 
-enum AUDIOFORMAT : u16{
-  AUDIOCONV_NONE = 0,
-  AUDIOCONV_SLE16 = 1,
-  AUDIOCONV_SLE16_F32 = 2,
-  AUDIOCONV_F32 = 3,
-  AUDIOCONV_F32_SLE16 = 4,
-};
-
-u32 Convert_SLE16_TO_F32(void* dst,void* src,u32 frame_count){
-  return sizeof(f32);
+u32 Convert_SLE16_TO_F32(void* dst, void* src, u32 frame_count) {
+	return sizeof(f32);
 }
 
-u32 Convert_F32_TO_SLE16(void* dst,void* src,u32 frame_count){
-  return sizeof(s16);
+u32 Convert_F32_TO_SLE16(void* dst, void* src, u32 frame_count) {
+	return sizeof(s16);
 }
 
-void Convert_NONE_SLE16(void* dst,void* src,u32 frame_count){
-  memcpy(dst,src,frame_count);
-  return sizeof(s16);
+u32 Convert_NONE_SLE16(void* dst, void* src, u32 frame_count) {
+	memcpy(dst, src, frame_count);
+	return sizeof(s16);
 }
 
-void Convert_NONE_F32(void* dst,void* src,u32 frame_count){
-  memcpy(dst,src,frame_count);
-  return sizeof(f32);
+u32 Convert_NONE_F32(void* dst, void* src, u32 frame_count) {
+	memcpy(dst, src, frame_count);
+	return sizeof(f32);
 }
 
 
-struct WASAPIDevice{
-  IMMDevice* device;
-  IAudioClient* audioclient;
-  IAudioRenderClient* renderclient;
-  u16 channels;
-  AUDIOFORMAT format;
-  void (conversion_function)(void*,void*,u32) = 0;
+struct WASAPIDevice {
+	IMMDevice* device;
+	IAudioClient* audioclient;
+	IAudioRenderClient* renderclient;
+	u32 channels;
+	u32 (*conversion_function)(void*, void*, u32);
 };
 
 static IMMDeviceEnumerator* device_enum = 0;
 
 //WASAPI allows us to change the sample rate of a stream but not the stream format
-WASAPIDevice TestCreateAudioDevice(const s8* device_name,u32 frequency,u32 channels,u32 format) {
+WASAPIDevice TestCreateAudioDevice(const s8* device_name, u32 frequency, u32 channels, u32 format) {
 
-  WASAPIDevice context = {};
+	WASAPIDevice context = {};
 
-  HRESULT res = 0;
+	HRESULT res = 0;
 
-  if (!audio_initialized) {
+	if (!audio_initialized) {
 
-    res = CoInitialize(0);
-    _kill("", res != S_OK);
+		res = CoInitialize(0);
+		_kill("", res != S_OK);
 
-    CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
-    IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
+		CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
+		IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 
-    res = CoCreateInstance(
-			   CLSID_MMDeviceEnumerator, 0,
-			   CLSCTX_INPROC_SERVER, IID_IMMDeviceEnumerator,
-			   (void**)&device_enum);
+		res = CoCreateInstance(
+			CLSID_MMDeviceEnumerator, 0,
+			CLSCTX_INPROC_SERVER, IID_IMMDeviceEnumerator,
+			(void**)&device_enum);
 
-    _kill("", res != S_OK);
-
-
-    audio_initialized = true;
-  }
-
-  if (device_name) {
-    _kill("we do not suppor this case yet\n", 1);
-  }
-
-  else {
-    res = device_enum->GetDefaultAudioEndpoint(eRender, eMultimedia, &context.device);
-  }
-
-  _kill("", res != S_OK);
-
-  res = context.device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, 0, (void**)&context.audioclient);
-
-  _kill("", res != S_OK);
-
-  WAVEFORMATEX* wv_format = 0;
-  context.audioclient->GetMixFormat(&wv_format);
+		_kill("", res != S_OK);
 
 
-  {
-    _kill("do not support this format\n", (((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat != KSDATAFORMAT_SUBTYPE_PCM) && ((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat != KSDATAFORMAT_SUBTYPE_IEEE_FLOAT);
+		audio_initialized = true;
+	}
 
-    if(format == A_FORMAT_S16LE && ((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat == KSDATAFORMAT_SUBTYPE_PCM){
-      context.conversion_function = Convert_NONE_SLE16;
-    }
+	if (device_name) {
+		_kill("we do not suppor this case yet\n", 1);
+	}
 
-    if(format == A_FORMAT_S16LE && ((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat = KSDATAFORMAT_SUBTYPE_IEEE_FLOAT){
-      context.conversion_function = Convert_SLE16_TO_F32;
-    }
+	else {
+		res = device_enum->GetDefaultAudioEndpoint(eRender, eMultimedia, &context.device);
+	}
 
-    _kill("for now\n",wv_format->nChannels != channels);
+	_kill("", res != S_OK);
 
-    context.channels = wv_format->nChannels;
-  }
+	res = context.device->Activate(__uuidof(IAudioClient), CLSCTX_ALL, 0, (void**)&context.audioclient);
 
-  _kill("", res != S_OK);
+	_kill("", res != S_OK);
 
-  //AUDCLNT_STREAMFLAGS_RATEADJUST  must be in shared mode only. lets you set the sample rate
-  //AUDCLNT_SHAREMODE_EXCLUSIVE Windows only
-  res = context.audioclient->Initialize(AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_RATEADJUST,
-					_buffersize,
-					0,//period size in - 100 nanoseconds. cannot be 0 in exclusive mode
-					wv_format, 0);
-
-  _kill("", res != S_OK);
-
-  if (wv_format->nSamplesPerSec != frequency) {
-    IAudioClockAdjustment* clockadj = 0;
-    IID IID_IAudioClockAdjustment = __uuidof(IAudioClockAdjustment);
-
-    res = context.audioclient->GetService(IID_IAudioClockAdjustment, (void**)&clockadj);
-    _kill("", res != S_OK);
-
-    res = clockadj->SetSampleRate(frequency);
-    _kill("", res != S_OK);
-  }
-
-  IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
-
-  res = context.audioclient->GetService(IID_IAudioRenderClient, (void**)&context.renderclient);
-  _kill("", res != S_OK);
+	WAVEFORMATEX* wv_format = 0;
+	context.audioclient->GetMixFormat(&wv_format);
 
 
-  //maybe we should start only after we do our first submission
-  res = context.audioclient->Start();
-  _kill("", res != S_OK);
+	{
+		_kill("do not support this format\n", (((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat != KSDATAFORMAT_SUBTYPE_PCM) && ((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat != KSDATAFORMAT_SUBTYPE_IEEE_FLOAT);
 
-  return context;
+		if (format == A_FORMAT_S16LE && ((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat == KSDATAFORMAT_SUBTYPE_PCM) {
+			context.conversion_function = Convert_NONE_SLE16;
+		}
+
+		if (format == A_FORMAT_S16LE && ((WAVEFORMATEXTENSIBLE*)wv_format)->SubFormat == KSDATAFORMAT_SUBTYPE_IEEE_FLOAT) {
+			context.conversion_function = Convert_SLE16_TO_F32;
+		}
+
+		_kill("for now\n", wv_format->nChannels != channels);
+
+		context.channels = wv_format->nChannels;
+	}
+
+	_kill("", res != S_OK);
+
+	//AUDCLNT_STREAMFLAGS_RATEADJUST  must be in shared mode only. lets you set the sample rate
+	//AUDCLNT_SHAREMODE_EXCLUSIVE Windows only
+	res = context.audioclient->Initialize(AUDCLNT_SHAREMODE_SHARED, AUDCLNT_STREAMFLAGS_RATEADJUST,
+		_buffersize,
+		0,//period size in - 100 nanoseconds. cannot be 0 in exclusive mode
+		wv_format, 0);
+
+	_kill("", res != S_OK);
+
+	if (wv_format->nSamplesPerSec != frequency) {
+		IAudioClockAdjustment* clockadj = 0;
+		IID IID_IAudioClockAdjustment = __uuidof(IAudioClockAdjustment);
+
+		res = context.audioclient->GetService(IID_IAudioClockAdjustment, (void**)&clockadj);
+		_kill("", res != S_OK);
+
+		res = clockadj->SetSampleRate(frequency);
+		_kill("", res != S_OK);
+	}
+
+	IID IID_IAudioRenderClient = __uuidof(IAudioRenderClient);
+
+	res = context.audioclient->GetService(IID_IAudioRenderClient, (void**)&context.renderclient);
+	_kill("", res != S_OK);
+
+
+	//maybe we should start only after we do our first submission
+	res = context.audioclient->Start();
+	_kill("", res != S_OK);
+
+	return context;
 }
 
 u32 TestDeviceGetWriteAvailable(WASAPIDevice context) {
 
-  u32 buffer_size_frames = 0;
-  u32 frames_locked = 0;
+	u32 buffer_size_frames = 0;
+	u32 frames_locked = 0;
 
-  auto res = context.audioclient->GetBufferSize(&buffer_size_frames);
-  _kill("", res != S_OK);
-
-
-  res = context.audioclient->GetCurrentPadding(&frames_locked);
-  _kill("", res != S_OK);
+	auto res = context.audioclient->GetBufferSize(&buffer_size_frames);
+	_kill("", res != S_OK);
 
 
-  return buffer_size_frames - frames_locked;
+	res = context.audioclient->GetCurrentPadding(&frames_locked);
+	_kill("", res != S_OK);
+
+
+	return buffer_size_frames - frames_locked;
 }
 
-void ConvertAndWrite(WASAPIDevice* context,void* data,u32 frame_count,void* dst_buffer){
+void ConvertAndWrite(WASAPIDevice* context, void* data, u32 frame_count, void* dst_buffer) {
 
 #define _reserved_frames (u32)_48ms2frames(36)
-  
-  u32 conversion_buffer[sizeof(u32) * _reserved_frames] = {};
-  _kill("exceeded conversion reserved conversion buffer\n",framecount > _reserved_frames);
 
-  auto samplesize = conversion_function(data,conversion_buffer,frame_count);
-  memcpy(dst_buffer,conversion_buffer,(samplesize * context.channels) * frame_count);
+	u32 conversion_buffer[sizeof(u32) * _reserved_frames] = {};
+	_kill("exceeded conversion reserved conversion buffer\n", frame_count > _reserved_frames);
+
+	auto samplesize = context->conversion_function(data, conversion_buffer, frame_count);
+	memcpy(dst_buffer, conversion_buffer, (samplesize * context->channels) * frame_count);
 }
 
-void TestPlayAudioDevice(WASAPIDevice context,void* data,u32 frame_count){
-  
-  s8* dst_buffer = 0;
-  context.renderclient->GetBuffer(frame_count, (BYTE**)&dst_buffer);
-  
-  ConvertAndWrite(&context,data,frame_count,dst_buffer);
-  
-  context.renderclient->ReleaseBuffer(frame_count,0);
+void TestPlayAudioDevice(WASAPIDevice context, void* data, u32 frame_count) {
+
+	s8* dst_buffer = 0;
+	context.renderclient->GetBuffer(frame_count, (BYTE**)&dst_buffer);
+
+	ConvertAndWrite(&context, data, frame_count, dst_buffer);
+
+	context.renderclient->ReleaseBuffer(frame_count, 0);
 }
 
 AAudioContext ACreateAudioDevice(const s8* device_name, u32 frequency, u32 channels,
