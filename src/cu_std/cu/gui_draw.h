3340ef5f98c8b2eba52a05a452fc7042d1e08fff
Edrick Hong <edrickhong@yahoo.com.sg>
2018-02-19 16:42:08 +0800

    fixed bug #1

--- src/cu_std/cu/gui_draw.h
+++ src/cu_std/cu/gui_draw.h
@@ -7,54 +7,54 @@
 
 
 struct GUIVertex{
-  f32 pos[3];
-  f32 uv[2];
-  f32 color[4];
+    f32 pos[3];
+    f32 uv[2];
+    f32 color[4];
 };
 
 struct GUIBoundingRect{
-  u16 x;
-  u16 y;
-  u16 width;
-  u16 height;
+    u16 x;
+    u16 y;
+    u16 width;
+    u16 height;
 };
 
 typedef Vector2 GUIVec2;
 typedef Vector3 GUIVec3;
 
 struct GUIDim2{
-  f32 w;
-  f32 h;
+    f32 w;
+    f32 h;
 };
 
 struct GUIFont{
-  VTextureContext texture;
-  u16 width;
-  u16 height;
-  VkDescriptorSet descset;
+    VTextureContext texture;
+    u16 width;
+    u16 height;
+    VkDescriptorSet descset;
 };
 
 enum GUIRenderMode{
-  GUI_RENDER_SOLID = 0,
-  GUI_RENDER_LINE = 1,
-  GUI_RENDER_TEXT = 2,
+    GUI_RENDER_SOLID = 0,
+    GUI_RENDER_LINE = 1,
+    GUI_RENDER_TEXT = 2,
 };
 
 enum GUICameraMode{
-  GUI_CAMERA_NONE = 0,
-  GUI_CAMERA_PERSPECTIVE = 1,
+    GUI_CAMERA_NONE = 0,
+    GUI_CAMERA_PERSPECTIVE = 1,
 };
 
 GUIFont GUICreateFontFromFile(const s8* filepath,VkCommandBuffer cmdbuffer,
-		      VDeviceContext* vdevice,VkQueue queue);
+                              VDeviceContext* vdevice,VkQueue queue);
 
 void GUIInit(VDeviceContext* vdevice,WWindowContext* window,
-	     VkRenderPass renderpass,VkQueue queue,VkCommandBuffer cmdbuffer,
-	     u32 vertexbinding_no = 0,
-	     GUIFont* fonthandle = 0);
+             VkRenderPass renderpass,VkQueue queue,VkCommandBuffer cmdbuffer,
+             u32 vertexbinding_no = 0,
+             GUIFont* fonthandle = 0);
 
 void GUIUpdate(WWindowContext* window,KeyboardState* keyboardstate,
-	       MouseState* mousestate,Matrix4b4 view,Matrix4b4 proj);
+               MouseState* mousestate,Matrix4b4 view,Matrix4b4 proj);
 
 void GUIDraw(VkCommandBuffer cmdbuffer);
 
@@ -68,9 +68,9 @@ void GUISetTitleColor(Color color);
 void GUISetTextColor(Color color);
 
 enum GUIWindowBehavoir{
-  GUIWINDOW_NONE = 0,
-  GUIWINDOW_NORESIZE = 1,
-  GUIWINDOW_NOMOVE = 2,
+    GUIWINDOW_NONE = 0,
+    GUIWINDOW_NORESIZE = 1,
+    GUIWINDOW_NOMOVE = 2,
 };
 
 void GUIBegin(const s8* title = 0,GUIVec2* pos = 0,GUIDim2* dim = 0);
@@ -87,7 +87,7 @@ logic GUITextField(const s8* label,const s8* buffer,logic fill_w = true,f32 w = 
 logic GUIButton(const s8* title);
 
 logic GUIComboBox(const s8* label,const s8** options_array,u32 options_count,u32* index,
-		  logic fill_w = true);
+                  logic fill_w = true);
 
 void GUIString(const s8* string);
 
@@ -98,12 +98,12 @@ void GUI3DScale(f32* x);
 void GUI3DRotate(f32* x,f32* y,f32* z);
 
 logic GUIHistogram(const s8* label_x,const s8* label_y,GUIVec2* data_array,u32 data_count,
-		   u32* out_entry_index,f32* max = 0,GUIDim2 dim = {0.5f,0.5f},u32* highlight_index = 0);
+                   u32* out_entry_index,f32* max = 0,GUIDim2 dim = {0.5f,0.5f},u32* highlight_index = 0);
 
 
 
 logic GUIProfileView(const s8* profilename,const DebugTable* table,u32* out_entry_index,
-		     GUIDim2 dim = {0.5f,0.5f});
+                     GUIDim2 dim = {0.5f,0.5f});
 
 logic GUIIsElementActive(const s8* element_name);
 
@@ -135,3 +135,5 @@ void SetGUIContext(GUIContext* context);
 
 
 void GUIGenFontFile(const s8* filepath,const s8* writepath,f32 fontsize);
+
+void GUIDebugGetCurrentHolder();
--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -8,313 +8,313 @@ _persist auto ustring = "patchthisvalueinatassetpacktime";
 
 
 s32 main(s32 argc,s8** argv){
-
+    
 #if  0
-        
+    
 #ifdef _WIN32
 #define _fontfile FONT_PATH(arial.ttf)
 #else
 #define _fontfile FONT_PATH(ubuntu-font-family/Ubuntu-B.ttf)
 #endif
-        
-  GUIGenFontFile(_fontfile,"Ubuntu-B.fbmp",100.0f);//200.0f
-  exit(0);
-        
+    
+    GUIGenFontFile(_fontfile,"Ubuntu-B.fbmp",100.0f);//200.0f
+    exit(0);
+    
 #endif
-
+    
 #if _debug && 0
-  {
-    
-    if(ustring[0] != '!'){
-      printf("binary has not been patched\n");
-      return -1;
+    {
+        
+        if(ustring[0] != '!'){
+            printf("binary has not been patched\n");
+            return -1;
+        }
+        
+        auto exec_size = *((u32*)&ustring[1]);
+        
+        printf("exec size %d\n",exec_size);
+        
+        auto file = FOpenFile(&argv[0][2],F_FLAG_READONLY);
+        
+        FSeekFile(file,exec_size,F_METHOD_START);
+        
+        s8 buffer[1024] = {};
+        u32 size;
+        
+        FRead(file,&size,sizeof(size));
+        FRead(file,&buffer[0],size);
+        
+        printf("%s\n",&buffer[0]);
+        
+        FCloseFile(file);
     }
-
-    auto exec_size = *((u32*)&ustring[1]);
-
-    printf("exec size %d\n",exec_size);
-
-    auto file = FOpenFile(&argv[0][2],F_FLAG_READONLY);
-
-    FSeekFile(file,exec_size,F_METHOD_START);
-
-    s8 buffer[1024] = {};
-    u32 size;
-
-    FRead(file,&size,sizeof(size));
-    FRead(file,&buffer[0],size);
-
-    printf("%s\n",&buffer[0]);
-
-    FCloseFile(file);
-  }
 #endif
     
-  _kill("ubo too big",sizeof(SkelUBO) > _kilobytes(64));
-  _kill("ubo too big",sizeof(PushConst) > 128);
-
-  void(*reload)(GameReloadData*) = 0;
-
-  //init code
-
-  InitInternalAllocator();
-  InitTAlloc(_megabytes(32));
+    _kill("ubo too big",sizeof(SkelUBO) > _kilobytes(64));
+    _kill("ubo too big",sizeof(PushConst) > 128);
     
-  SetupData((void**)&pdata,(void**)&gdata);
-
-  pdata->window = WCreateWindow("Cu",W_CREATE_NORESIZE,100,100,1280,720);
+    void(*reload)(GameReloadData*) = 0;
     
-  TInitTimer();
-  INIT_DEBUG_TIMER();
+    //init code
     
-  VCreateInstance("eengine",true,1,0,0);
+    InitInternalAllocator();
+    InitTAlloc(_megabytes(32));
     
-  pdata->vdevice = VCreateDeviceContext(&pdata->window);
+    SetupData((void**)&pdata,(void**)&gdata);
     
-  pdata->swapchain = VCreateSwapchainContext(&pdata->vdevice,2,pdata->window,
-					     VSYNC_NORMAL);
-
-  InitAssetAllocator(_gigabytes(1),_megabytes(500),&pdata->vdevice,
-		     &pdata->swapchain);
-
-  pdata->present_fence = VCreateFence(&pdata->vdevice,(VkFenceCreateFlagBits)0);
+    pdata->window = WCreateWindow("Cu",W_CREATE_NORESIZE,100,100,1280,720);
     
-  printf("swapchain count %d\n",pdata->swapchain.image_count);
+    TInitTimer();
+    INIT_DEBUG_TIMER();
     
-  pdata->root_queue = VGetQueue(&pdata->vdevice,VQUEUETYPE_ROOT);
+    VCreateInstance("eengine",true,1,0,0);
     
-  pdata->drawcmdbuffer =
-    CreateThreadRenderData(&pdata->vdevice);
+    pdata->vdevice = VCreateDeviceContext(&pdata->window);
     
-  pdata->transfer_queue = VGetQueue(&pdata->vdevice,VQUEUETYPE_ROOT);
-
-  //MARK: just reuse a cmdbuffer
-  VkCommandBuffer transfercmdbuffer =
-    VAllocateCommandBuffer(&pdata->vdevice,pdata->drawcmdbuffer.pool,
-			   VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+    pdata->swapchain = VCreateSwapchainContext(&pdata->vdevice,2,pdata->window,
+                                               VSYNC_NORMAL);
     
-  //MARK: Handle case where a compute queue doesn't exist
-  pdata->compute_queue = VGetQueue(&pdata->vdevice,VQUEUETYPE_COMPUTE);
+    InitAssetAllocator(_gigabytes(1),_megabytes(500),&pdata->vdevice,
+                       &pdata->swapchain);
     
-  pdata->waitacquireimage_semaphore = VCreateSemaphore(&pdata->vdevice);
-  pdata->waitfinishrender_semaphore = VCreateSemaphore(&pdata->vdevice);
+    pdata->present_fence = VCreateFence(&pdata->vdevice,(VkFenceCreateFlagBits)0);
     
-  pdata->renderpass = SetupRenderPass(&pdata->vdevice,pdata->swapchain);
-  
-  SetupFrameBuffers(&pdata->vdevice,transfercmdbuffer,pdata->transfer_queue,
-		    pdata->renderpass,&pdata->swapchain);
-
-  pdata->skel_ubo = VCreateUniformBufferContext(&pdata->vdevice,sizeof(SkelUBO[64]),false);
-      
-  
-  {
-    pdata->submit_audiobuffer.size_frames =
-      (u32)(_48ms2frames(_targetframerate) + _48ms2frames(8));
-        
-    pdata->submit_audiobuffer.size =
-      pdata->submit_audiobuffer.size_frames * sizeof(s16) * 2;
-        
-    pdata->submit_audiobuffer.data = alloc(pdata->submit_audiobuffer.size); 
-  }
-      
-  pdata->audio =
-    ACreateAudioDevice(A_DEVICE_DEFAULT,48000,2,A_FORMAT_S16LE);
-
-  InitSceneContext(pdata,transfercmdbuffer,pdata->transfer_queue);
+    printf("swapchain count %d\n",pdata->swapchain.image_count);
     
-  //Kickoff worker threads
-  {
-
-    pdata->worker_sem = TCreateSemaphore();
-    pdata->main_sem = TCreateSemaphore();
+    pdata->root_queue = VGetQueue(&pdata->vdevice,VQUEUETYPE_ROOT);
     
-    auto info = TAlloc(Threadinfo,1);
-    memset(info,sizeof(Threadinfo),0);
+    pdata->drawcmdbuffer =
+        CreateThreadRenderData(&pdata->vdevice);
     
-    info->this_sem = pdata->worker_sem;
-    info->main_sem = pdata->main_sem;
-    info->queue = &pdata->threadqueue;
-    info->rendercontext = &pdata->rendercontext;
-    info->vdevicecontext = pdata->vdevice;
-    pdata->fetchqueue = {};
-    pdata->fetchqueue.buffer = ((ThreadFetchBatch*)alloc(_FetchqueueSize *
-							 sizeof(ThreadFetchBatch)));
-    info->fetchqueue = &pdata->fetchqueue;
+    pdata->transfer_queue = VGetQueue(&pdata->vdevice,VQUEUETYPE_ROOT);
     
-    pdata->threadcount = DeployAllThreads(info);
-  }
+    //MARK: just reuse a cmdbuffer
+    VkCommandBuffer transfercmdbuffer =
+        VAllocateCommandBuffer(&pdata->vdevice,pdata->drawcmdbuffer.pool,
+                               VK_COMMAND_BUFFER_LEVEL_PRIMARY);
+    
+    //MARK: Handle case where a compute queue doesn't exist
+    pdata->compute_queue = VGetQueue(&pdata->vdevice,VQUEUETYPE_COMPUTE);
+    
+    pdata->waitacquireimage_semaphore = VCreateSemaphore(&pdata->vdevice);
+    pdata->waitfinishrender_semaphore = VCreateSemaphore(&pdata->vdevice);
+    
+    pdata->renderpass = SetupRenderPass(&pdata->vdevice,pdata->swapchain);
+    
+    SetupFrameBuffers(&pdata->vdevice,transfercmdbuffer,pdata->transfer_queue,
+                      pdata->renderpass,&pdata->swapchain);
+    
+    pdata->skel_ubo = VCreateUniformBufferContext(&pdata->vdevice,sizeof(SkelUBO[64]),false);
     
     
-  pdata->rendercmdbuffer_array =
-    CreatePrimaryRenderCommandbuffer(&pdata->vdevice,
-				     pdata->drawcmdbuffer.pool,pdata->swapchain.image_count);
-
-  GUIInit(&pdata->vdevice,&pdata->window,pdata->renderpass,pdata->transfer_queue,
-	  transfercmdbuffer);
-
-  GameInitData initdata = {
-    &pdata->scenecontext,gdata,&pdata->window,pdata->vdevice,pdata->renderpass,
-    transfercmdbuffer,pdata->transfer_queue
-  };
-
-    
-  void (*gameinit_funptr)(GameInitData*);
-  pdata->lib = InitGameLibrary((void**)(&gameinit_funptr));
-    
-    
-  gameinit_funptr(&initdata);
-    
-  pdata->deltatime = 0;
-    
-  ResetTransferBuffer();
-
-
-  //MARK: keep the obj buffer permanently mapped
-  vkMapMemory(pdata->vdevice.device,pdata->skel_ubo.memory,
-	      0,pdata->skel_ubo.size,0,(void**)&pdata->objupdate_ptr);
-    
-  while(gdata->running){
-        
     {
-      
-      pdata->objupdate_count = 0;
-      gui_draw_is_locked = 0;
-
-      Clear(&pdata->rendercontext);
-
-      GUIUpdate(&pdata->window,&pdata->keyboardstate,&pdata->mousestate,
-		pdata->view,pdata->proj);
-
-      GUIBegin();
-
-      BUILDGUIGRAPH(gdata->draw_profiler);
-            
-      TimeSpec start,end;
-
-      MASTERTIMEBLOCKSTART(Aqua);
-            
-      GetTime(&start);
-            
-      {
-	      
-	EXECTIMEBLOCK(Black);
-
-	{
-	  TIMEBLOCKTAGGED("AcquireImage",Orange);
-	  vkAcquireNextImageKHR(pdata->vdevice.device,
-				pdata->swapchain.swap,0xFFFFFFFFFFFFFFFF,
-				pdata->waitacquireimage_semaphore,
-				0,(u32*)&pdata->swapchain.image_index);  
-	}
-
-	
-
-	UpdateAllocatorTimeStamp();
-	Clear(&pdata->threadqueue);
-                
-	ProcessEvents(&pdata->window,&pdata->keyboardstate,&pdata->mousestate,
-		      &gdata->running);
-                
-	{
-	  TIMEBLOCKTAGGED("RELOADLIB",Firebrick);
-	  pdata->lib = ReloadGameLibrary(pdata->lib,(void**)&reload,&pdata->scenecontext);
-	}
-                
-                
-	if(reload){
-
-	  auto context = &pdata->scenecontext;
-                    
-	  GameReloadData reloaddata = {
-	    gdata,pdata->vdevice,pdata->renderpass,pdata->window,context,GetGUIContext(),
-	    GetAAllocatorContext()
-	  };
-                    
-	  vkDeviceWaitIdle(pdata->vdevice.device);
-                    
-	  reload(&reloaddata);
-	  reload = 0;
-
-	  CompileAllPipelines(pdata);
-	}
-                
-	ThreadLinearBlendRes blendres_array[24] = {};
-	u32 linearblend_count = 0;
-	u32 audio_count = 0;
-	EntityAudioData* audio_data;
-	
-                
-	if(pdata->lib.updaterender){
-
-	  auto context = &pdata->scenecontext;
-	  context->prev_frametime = pdata->deltatime;
-	  context->keyboardstate = &pdata->keyboardstate;
-	  context->mousestate = &pdata->mousestate;
-	  context->audiocontext = &audio_data;
-	  context->audiocontext_count = &audio_count;
-                    
-	  auto ptr =
-	    (void (*)(SceneContext*))pdata->lib.updaterender;
-                    
-	  ptr(context);
-	}
-
-	ProcessDrawList();
-                
-	auto frames = AAudioDeviceWriteAvailable(pdata->audio);
-                
-	if(frames >= pdata->submit_audiobuffer.size_frames){
-                    
-	  MixAudioLayout args = {&pdata->submit_audiobuffer,audio_data,audio_count};
-                    
-	  PushThreadWorkQueue(&pdata->threadqueue,
-			      MixAudio,(void*)&args,pdata->worker_sem);
-	  
-	}
-
-	GUIEnd();
-                
-	BuildRenderCommandBuffer(pdata);
-                
-	MainThreadDoWorkQueue(&pdata->threadqueue,0);
-
-	//FIXME: if there is corruption, it is either because: 1. Flush is too slow,
-	//2. MainThreadDoWorkQueue is not synced
-	//3. Someone is writing into another person's data
-	ProcessObjUpdateList();
-                
-	PresentBuffer(pdata);
-      }
-            
-      GetTime(&end);
-            
-      auto diff = GetTimeDifferenceMS(start,end);
-            
-            
-      f32 sleeptime = (_targetframerate - diff);
-            
-            
-      if(sleeptime > 0 ){
-	TIMEBLOCKTAGGED("Sleep",DarkGray);
-	SleepMS(sleeptime);
-      }
-            
-      GetTime(&end);
-            
-      pdata->deltatime = GetTimeDifferenceMS(start,end);
-            
+        pdata->submit_audiobuffer.size_frames =
+            (u32)(_48ms2frames(_targetframerate) + _48ms2frames(8));
+        
+        pdata->submit_audiobuffer.size =
+            pdata->submit_audiobuffer.size_frames * sizeof(s16) * 2;
+        
+        pdata->submit_audiobuffer.data = alloc(pdata->submit_audiobuffer.size); 
     }
     
-  }
+    pdata->audio =
+        ACreateAudioDevice(A_DEVICE_DEFAULT,48000,2,A_FORMAT_S16LE);
     
-  return 0;
+    InitSceneContext(pdata,transfercmdbuffer,pdata->transfer_queue);
+    
+    //Kickoff worker threads
+    {
+        
+        pdata->worker_sem = TCreateSemaphore();
+        pdata->main_sem = TCreateSemaphore();
+        
+        auto info = TAlloc(Threadinfo,1);
+        memset(info,sizeof(Threadinfo),0);
+        
+        info->this_sem = pdata->worker_sem;
+        info->main_sem = pdata->main_sem;
+        info->queue = &pdata->threadqueue;
+        info->rendercontext = &pdata->rendercontext;
+        info->vdevicecontext = pdata->vdevice;
+        pdata->fetchqueue = {};
+        pdata->fetchqueue.buffer = ((ThreadFetchBatch*)alloc(_FetchqueueSize *
+                                                             sizeof(ThreadFetchBatch)));
+        info->fetchqueue = &pdata->fetchqueue;
+        
+        pdata->threadcount = DeployAllThreads(info);
+    }
+    
+    
+    pdata->rendercmdbuffer_array =
+        CreatePrimaryRenderCommandbuffer(&pdata->vdevice,
+                                         pdata->drawcmdbuffer.pool,pdata->swapchain.image_count);
+    
+    GUIInit(&pdata->vdevice,&pdata->window,pdata->renderpass,pdata->transfer_queue,
+            transfercmdbuffer);
+    
+    GameInitData initdata = {
+        &pdata->scenecontext,gdata,&pdata->window,pdata->vdevice,pdata->renderpass,
+        transfercmdbuffer,pdata->transfer_queue
+    };
+    
+    
+    void (*gameinit_funptr)(GameInitData*);
+    pdata->lib = InitGameLibrary((void**)(&gameinit_funptr));
+    
+    
+    gameinit_funptr(&initdata);
+    
+    pdata->deltatime = 0;
+    
+    ResetTransferBuffer();
+    
+    
+    //MARK: keep the obj buffer permanently mapped
+    vkMapMemory(pdata->vdevice.device,pdata->skel_ubo.memory,
+                0,pdata->skel_ubo.size,0,(void**)&pdata->objupdate_ptr);
+    
+    while(gdata->running){
+        
+        {
+            
+            pdata->objupdate_count = 0;
+            gui_draw_is_locked = 0;
+            
+            Clear(&pdata->rendercontext);
+            
+            GUIUpdate(&pdata->window,&pdata->keyboardstate,&pdata->mousestate,
+                      pdata->view,pdata->proj);
+            
+            GUIBegin();
+            
+            BUILDGUIGRAPH(gdata->draw_profiler);
+            
+            TimeSpec start,end;
+            
+            MASTERTIMEBLOCKSTART(Aqua);
+            
+            GetTime(&start);
+            
+            {
+                
+                EXECTIMEBLOCK(Black);
+                
+                {
+                    TIMEBLOCKTAGGED("AcquireImage",Orange);
+                    vkAcquireNextImageKHR(pdata->vdevice.device,
+                                          pdata->swapchain.swap,0xFFFFFFFFFFFFFFFF,
+                                          pdata->waitacquireimage_semaphore,
+                                          0,(u32*)&pdata->swapchain.image_index);  
+                }
+                
+                
+                
+                UpdateAllocatorTimeStamp();
+                Clear(&pdata->threadqueue);
+                
+                ProcessEvents(&pdata->window,&pdata->keyboardstate,&pdata->mousestate,
+                              &gdata->running);
+                
+                {
+                    TIMEBLOCKTAGGED("RELOADLIB",Firebrick);
+                    pdata->lib = ReloadGameLibrary(pdata->lib,(void**)&reload,&pdata->scenecontext);
+                }
+                
+                
+                if(reload){
+                    
+                    auto context = &pdata->scenecontext;
+                    
+                    GameReloadData reloaddata = {
+                        gdata,pdata->vdevice,pdata->renderpass,pdata->window,context,GetGUIContext(),
+                        GetAAllocatorContext()
+                    };
+                    
+                    vkDeviceWaitIdle(pdata->vdevice.device);
+                    
+                    reload(&reloaddata);
+                    reload = 0;
+                    
+                    CompileAllPipelines(pdata);
+                }
+                
+                ThreadLinearBlendRes blendres_array[24] = {};
+                u32 linearblend_count = 0;
+                u32 audio_count = 0;
+                EntityAudioData* audio_data;
+                
+                
+                if(pdata->lib.updaterender){
+                    
+                    auto context = &pdata->scenecontext;
+                    context->prev_frametime = pdata->deltatime;
+                    context->keyboardstate = &pdata->keyboardstate;
+                    context->mousestate = &pdata->mousestate;
+                    context->audiocontext = &audio_data;
+                    context->audiocontext_count = &audio_count;
+                    
+                    auto ptr =
+                        (void (*)(SceneContext*))pdata->lib.updaterender;
+                    
+                    ptr(context);
+                }
+                
+                ProcessDrawList();
+                
+                auto frames = AAudioDeviceWriteAvailable(pdata->audio);
+                
+                if(frames >= pdata->submit_audiobuffer.size_frames){
+                    
+                    MixAudioLayout args = {&pdata->submit_audiobuffer,audio_data,audio_count};
+                    
+                    PushThreadWorkQueue(&pdata->threadqueue,
+                                        MixAudio,(void*)&args,pdata->worker_sem);
+                    
+                }
+                
+                GUIEnd();
+                
+                BuildRenderCommandBuffer(pdata);
+                
+                MainThreadDoWorkQueue(&pdata->threadqueue,0);
+                
+                //FIXME: if there is corruption, it is either because: 1. Flush is too slow,
+                //2. MainThreadDoWorkQueue is not synced
+                //3. Someone is writing into another person's data
+                ProcessObjUpdateList();
+                
+                PresentBuffer(pdata);
+            }
+            
+            GetTime(&end);
+            
+            auto diff = GetTimeDifferenceMS(start,end);
+            
+            
+            f32 sleeptime = (_targetframerate - diff);
+            
+            
+            if(sleeptime > 0 ){
+                TIMEBLOCKTAGGED("Sleep",DarkGray);
+                SleepMS(sleeptime);
+            }
+            
+            GetTime(&end);
+            
+            pdata->deltatime = GetTimeDifferenceMS(start,end);
+            
+        }
+        
+    }
+    
+    return 0;
     
 }
 
 
 /*
   TODO: 
-
+  
   Compile all assets into an adb file (asset data base). We will build a function a constexpr
   function at compile time which translates filepaths to indices and an adb file w raw data.
   We will keep the adb file open at all times and just read from the offset as required.
@@ -330,10 +330,10 @@ s32 main(s32 argc,s8** argv){
   Support drawing multiple objects off a single buffer(use vert & index buffer offsets into buffer)
   implement quaternion double cover
   Implement Dual quaternion blending in MDF
-
+  
   FIXME:
   TAlloc calls. I think there are some corruption issues caused by these calls
-
+  
   main:
   src/engine/main.h:  auto pushconst = TAlloc(PushConst,1);
   src/engine/main.h:  auto cmdbuffers = TAlloc(VkCommandBuffer,count);
@@ -345,9 +345,9 @@ s32 main(s32 argc,s8** argv){
   
   main(from game):  
   src/engine/main.h:  auto orientation = TAlloc(Matrix4b4,1);
-
-
-
+  
+  
+  
 WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\desktop\cu\src\engine\main.h SetObjectOrientation 1282
 WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\desktop\cu\src\engine\main.h DispatchSkelLinearBlend 1592
 WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\desktop\cu\src\engine\main.h DispatchSkelLinearBlend 1591
@@ -355,5 +355,5 @@ WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\
 WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\desktop\cu\src\engine\main.h DispatchRenderContext 418
 WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\desktop\cu\src\engine\main.h GetCmdBufferArray 453
 WARNING: Raw ptr, no bounds checking can be done for passed ptr : c:\users\user\desktop\cu\src\engine\main.h ProcessObjUpdateList 968
-  
+
 */
