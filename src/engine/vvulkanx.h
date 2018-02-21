b931742e761a28c87b5aea6a0a39810adb32c1ed
Edrick Hong <edrickhong@yahoo.com.sg>
2018-02-21 21:39:36 +0800

    prepping to support proper lights. rn refining the shader portion of pipeline creation to make it cleaner and to allow using the same binding points in many shaders

--- src/engine/vvulkanx.h
+++ src/engine/vvulkanx.h
@@ -7,65 +7,48 @@
   The base vvulkan implementation will be as independent from the rest of the other tools as 
   possible*/
 
-void _ainline VPushBackShaderPipelineSpec(VGraphicsPipelineSpec* spec,const s8* filepath,
-					  VkShaderStageFlagBits stage,void** shaderdataptr,
-					  VkSpecializationInfo specialization = {}){
-
-  auto file = FOpenFile(filepath,F_FLAG_READONLY);
-
-  ptrsize shadersize;
-  void* shaderdata = FReadFileToBuffer(file,&shadersize);
-  
-
-  VPushBackShaderPipelineSpec(spec,shaderdata,shadersize,stage,specialization);
-
-  *shaderdataptr = shaderdata;
-
-  FCloseFile(file);
-}
-
 void _ainline VSetComputePipelineSpecShader(VComputePipelineSpec* spec,const s8* filepath,
-					  void** shaderdataptr,VkSpecializationInfo specialization = {}){
-
-  auto file = FOpenFile(filepath,F_FLAG_READONLY);
-
-  ptrsize shadersize;
-  void* shaderdata = FReadFileToBuffer(file,&shadersize);
-  
-
-  VSetComputePipelineSpecShader(spec,shaderdata,shadersize,specialization);
-
-  *shaderdataptr = shaderdata;
-
-  FCloseFile(file);
+                                            void** shaderdataptr,VkSpecializationInfo specialization = {}){
+    
+    auto file = FOpenFile(filepath,F_FLAG_READONLY);
+    
+    ptrsize shadersize;
+    void* shaderdata = FReadFileToBuffer(file,&shadersize);
+    
+    
+    VSetComputePipelineSpecShader(spec,shaderdata,shadersize,specialization);
+    
+    *shaderdataptr = shaderdata;
+    
+    FCloseFile(file);
 }
 
 void VDescPushBackPoolSpecX(VDescriptorPoolSpec* spec,const SPXData* spx_array,
-			    u32 spx_count,u32 descset_count = 1);
+                            u32 spx_count,u32 descset_count = 1);
 
 
 u32 VPushBackShaderPipelineSpecX(VGraphicsPipelineSpec* spec,const SPXData* spx,
-				  VkSpecializationInfo specialization = {},
-				  u32 vert_bindingno = 0,u32 inst_bindingno = 1);
+                                 VkSpecializationInfo specialization = {},
+                                 u32 vert_bindingno = 0,u32 inst_bindingno = 1);
 
 void VPushBackShaderArrayPipelineSpecX(VGraphicsPipelineSpec* spec,
-				       SPXData* spx_array,u32 spx_count,
-				       u32 vert_bindingno = 0,u32 inst_bindingno = 1);
+                                       SPXData* spx_array,u32 spx_count,
+                                       u32 vert_bindingno = 0,u32 inst_bindingno = 1);
 
 
 VkDescriptorSetLayout VCreateDescriptorSetLayoutX(
-						  const  VDeviceContext* _restrict vdevice,
-						 SPXData* spx_array,u32 spx_count,u32 descset_no);
+const  VDeviceContext* _restrict vdevice,
+SPXData* spx_array,u32 spx_count,u32 descset_no);
 
 
 VkPipelineLayout VCreatePipelineLayoutX(const  VDeviceContext* _restrict vdevice,
-					VkDescriptorSetLayout* descriptorset_array,
-					u32 descriptorset_count,
-					SPXData* spx_array,
-					u32 spx_count);
+                                        VkDescriptorSetLayout* descriptorset_array,
+                                        u32 descriptorset_count,
+                                        SPXData* spx_array,
+                                        u32 spx_count);
 
 
 
 
 VkDescriptorPool VCreateDescriptorPoolX(VDeviceContext* _in_ vdevice,
-				       VDescriptorPoolSpec poolspec,u32 flags = 0);
+                                        VDescriptorPoolSpec poolspec,u32 flags = 0);
