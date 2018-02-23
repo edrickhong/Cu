5b736097030876e5b462d506f60ce783c9e8b474
Edrick Hong <edrickhong@yahoo.com.sg>
2018-02-23 18:24:59 +0800

    defining new pipeline creation interfaces

--- src/engine/vvulkanx.cpp
+++ src/engine/vvulkanx.cpp
@@ -47,13 +47,13 @@ u32 VPushBackShaderPipelineSpecX(VGraphicsPipelineSpec* spec,const SPXData* spx,
         if(spx->instlayout.size){
             
             VPushBackVertexSpecDesc(spec,inst_bindingno,spx->instlayout.size,
-                                    VK_VERTEX_INPUT_RATE_VERTEX);
+                                    VK_VERTEX_INPUT_RATE_INSTANCE);
             
             for(u32 i = 0; i < spx->instlayout.entry_count; i++){
                 
                 const auto entry = &spx->instlayout.entry_array[i];
                 
-                VPushBackVertexSpecAttrib(spec,vert_bindingno,
+                VPushBackVertexSpecAttrib(spec,inst_bindingno,
                                           entry->format,entry->size);
                 
                 format_array[format_count] = entry->format;
@@ -199,4 +199,167 @@ VkDescriptorPool VCreateDescriptorPoolX(VDeviceContext* _in_ vdevice,
                                         VDescriptorPoolSpec poolspec,u32 flags){
     
     return VCreateDescriptorPool(vdevice,poolspec,flags,poolspec.desc_count);
+}
+
+void InternalPushBackVertexAttrib(SPX_GraphicsShaderObject* obj,u32 binding_no,VkFormat format,u32 attrib_size){
+    
+    _kill("cannot push more vert attrib\n",obj->vert_attrib_count >= _arraycount(obj->vert_attrib_array));
+    
+    u32 offset = 0;
+    
+    for(u32 i = 0; i < obj->vert_attrib_count; i++){
+        
+        if(binding_no == obj->vert_attrib_array[i].format){
+            offset += obj->vert_attrib_size_array[i];
+        }
+    }
+    
+    obj->vert_attrib_array[obj->vert_attrib_count] = {obj->vert_attrib_count,binding_no,format,offset};
+    obj->vert_attrib_count++;
+    
+}
+
+void InternalPushBackVertexDesc(SPX_GraphicsShaderObject* obj,u32 binding_no,u32 vert_size,VkVertexInputRate inputrate){
+    
+    _kill("cannot push more vert desc\n",obj->vert_desc_count >= _arraycount(obj->vert_desc_array));
+    
+    obj->vert_desc_array[obj->vert_desc_count] = {binding_no,vert_size,inputrate};
+    obj->vert_desc_count++;
+    
+}
+
+void InternalHandleVertexBuilding(SPX_GraphicsShaderObject* obj,SPXData* spx,u32 vert_binding_no,u32 inst_binding_no){
+    
+    if(spx->type != VK_SHADER_STAGE_VERTEX_BIT){
+        return;
+    }
+    
+    VkFormat format_array[32] = {};
+    u32 format_count = 0;
+    u64 vert_hash = 0;
+    
+    
+    InternalPushBackVertexDesc(obj,vert_binding_no,spx->vlayout.size,VK_VERTEX_INPUT_RATE_VERTEX);
+    
+    for(u32 j = 0; j < spx->vlayout.entry_count; j++){
+        
+        const auto entry = &spx->vlayout.entry_array[j];
+        
+        InternalPushBackVertexAttrib(obj,vert_binding_no,entry->format,entry->size);
+        
+        format_array[format_count] = entry->format;
+        format_count++;
+        _kill("not enough space\n",format_count >= _arraycount(format_array));
+    }
+    
+    if(spx->instlayout.size){
+        
+        InternalPushBackVertexDesc(obj,inst_binding_no,spx->instlayout.size,VK_VERTEX_INPUT_RATE_INSTANCE);
+        
+        for(u32 j = 0; j < spx->instlayout.entry_count; j++){
+            
+            const auto entry = &spx->instlayout.entry_array[j];
+            
+            InternalPushBackVertexAttrib(obj,inst_binding_no,entry->format,entry->size);
+            
+            format_array[format_count] = entry->format;
+            format_count++;
+            _kill("not enough space\n",format_count >= _arraycount(format_array));
+        }
+        
+    }
+    
+    vert_hash = VPipelineVertHash(format_array,format_count);
+    
+    obj->vert_hash = vert_hash;
+}
+
+#include "aassettools.h"
+
+void DebugCompareSet(SPX_GraphicsShaderObject::DescSetEntry* set,DescEntry* entry){
+    
+    for(u32 i = 0; i < set->element_count; i++){
+        auto el = &set->element_array[i];
+        
+        if(el->binding_no == entry->bind){
+            _kill("elements do not match\n",(el->type != entry->type) || (el->array_count != entry->array_count));
+            return;
+        }
+    }
+    
+    _kill("element not found\n",1);
+}
+
+void SetAddElement(SPX_GraphicsShaderObject::DescSetEntry* set,DescEntry* entry){
+    
+    _kill("max desc set element reached\n",set->element_count >= _arraycount(set->element_array));
+    
+    set->element_array[set->element_count] = {entry->type,entry->bind,entry->array_count};
+    set->element_count++;
+}
+
+SPX_GraphicsShaderObject::DescSetEntry* GetDescSet(SPX_GraphicsShaderObject* obj,u32 set_no){
+    
+    for(u32 i = 0; i < obj->descset_count; i++){
+        auto entry = &obj->descset_array[i];
+        if(set_no == entry->set_no){
+            return entry;
+        }
+    }
+    
+    _kill("max desc set reached\n",obj->descset_count >= _arraycount(obj->descset_array));
+    
+    auto entry = &obj->descset_array[obj->descset_count];
+    obj->descset_count++;
+    
+    return entry;
+}
+
+void ConstructDescSets(SPX_GraphicsShaderObject* obj,SPXData* spx){
+    
+    for(u32 i = 0; i < spx->dlayout.entry_count; i++){
+        
+        auto entry = &spx->dlayout.entry_array[i];
+        
+        auto set = GetDescSet(obj,entry->set);
+        
+        set->shader_stage |= spx->type;
+        
+        //set has not been defined
+        if(set->shader_stage == spx->type){
+            SetAddElement(set,entry);
+        }
+        
+#if _debug
+        //set has been defined. Make sure it is the same across all shaders
+        else{
+            DebugCompareSet(set,entry);
+        }
+#endif
+    }
+    
+}
+
+
+SPX_GraphicsShaderObject MakeShaderObjectSPX(SPXData* spx_array,
+                                             u32 spx_count,u32 vert_binding_no,u32 inst_binding_no){
+    
+    SPX_GraphicsShaderObject obj = {};
+    
+    for(u32 i = 0; i < spx_count; i++){
+        
+        auto spx = &spx_array[i];
+        
+        InternalHandleVertexBuilding(&obj,spx,vert_binding_no,inst_binding_no);
+        
+        ConstructDescSets(&obj,spx);
+        
+    }
+    
+    _kill("no vertex shader found\n",!obj.vert_hash);
+    return obj;
+}
+
+void VPushBackDescriptorPoolX(VDescriptorPoolSpec* spec,SPX_GraphicsShaderObject* obj,u32 descset_count,u32 desc_set){
+    
 }
