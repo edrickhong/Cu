585ebf564f4181812b250cf6339a7b0ecd708798
Edrick Hong <edrickhong@yahoo.com.sg>
2018-02-28 21:42:34 +0800

    moved back the extended functionality functions back to where they should be (They rely on the SPX format to generate the data they need to do creation)

--- src/engine/vvulkanx.h
+++ src/engine/vvulkanx.h
@@ -16,3 +16,10 @@ VkDescriptorPool VCreateDescriptorPoolX(VDeviceContext* _in_ vdevice,
                                         VDescriptorPoolSpec poolspec,u32 flags = 0);
 
 VShaderObj VMakeShaderObjSPX(SPXData* spx_array,u32 spx_count,VkSpecializationInfo* spec_array = 0,u32 spec_count = 0,u32 vert_binding_no = 0,u32 inst_binding_no = 1);
+
+VkDescriptorSetLayout VCreateDescriptorSetLayout(const  VDeviceContext* vdevice,VShaderObj* obj,u32 descset_no);
+
+VkPipelineLayout VCreatePipelineLayout(const  VDeviceContext* _restrict vdevice,
+                                       VkDescriptorSetLayout* descriptorset_array,
+                                       u32 descriptorset_count,
+                                       VShaderObj* obj);
