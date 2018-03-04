da212cf0790934dd7d8125cde57b813fc394f00d
Edrick Hong <edrickhong@yahoo.com.sg>
2018-03-04 13:42:13 +0800

    prepping for editable lights. updates to ubo are now done on push instead of on flush

--- src/game/gamecomp.h
+++ src/game/gamecomp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 typedef u32 ObjectID;
+typedef u32 LightID;
 typedef u32 AnimationID;
 typedef u32 ModelID;
 typedef u32 TextureID;
@@ -9,24 +10,30 @@ typedef u32 MaterialID;
 
 //NOTE: we should not be able to edit core formats. this is because the engine depends not changing
 struct REFLCOMPONENT EntityAnimationData{
-  ObjectID id;
-  AnimationID animdata_id;
-  u16 animationindex;
-  u16 islooping;
-  f32 animationtime;
-  f32 speed;
+    ObjectID id;
+    AnimationID animdata_id;
+    u16 animationindex;
+    u16 islooping;
+    f32 animationtime;
+    f32 speed;
 };
 
 struct REFLCOMPONENT EntityDrawData{
-  ObjectID id;
-  ModelID model;
-  MaterialID material;
-  RenderGroupIndex group;
+    ObjectID id;
+    ModelID model;
+    MaterialID material;
+    RenderGroupIndex group;
 };
 
 struct REFLCOMPONENT EntityAudioData{
-  ObjectID id;
-  AudioAssetHandle audioasset;
-  u16 islooping = 0;
-  u16 toremove = 0;
+    ObjectID id;
+    AudioAssetHandle audioasset;
+    u16 islooping = 0;
+    u16 toremove = 0;
+};
+
+struct REFLCOMPONENT PointLight{
+    ObjectID id;
+    Color color;
+    f32 intensity;
 };
