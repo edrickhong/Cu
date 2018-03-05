a29505eae22515356f15463df3335f8d64ebbb9a
Edrick Hong <edrickhong@yahoo.com.sg>
2018-03-05 19:53:05 +0800

    added attenuation. maybe we should prefer spherical attenuation

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -66,7 +66,7 @@ s32 main(s32 argc,s8** argv){
     TInitTimer();
     INIT_DEBUG_TIMER();
     
-    VCreateInstance("eengine",true,1,0,0);
+    VCreateInstance("eengine",false,1,0,0);
     
     pdata->vdevice = VCreateDeviceContext(&pdata->window);
     
