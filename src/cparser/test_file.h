f72eba7f06177690969a1045cdf8b8fa3fb20abb
Edrick Hong <edrickhong@yahoo.com.sg>
2018-03-18 19:27:42 +0800

    way better c parser in progress. we now properly santiize strings of redundancy. evaluator in progress, but it should allow us to parse structs and functions

--- src/cparser/test_file.h
+++ src/cparser/test_file.h
@@ -1,4 +1,6 @@
 #define REFLCOMPONENT
+#define REFLSTRUCT(TAG)
+#define REFLFUNC(TAG)
 
 /*
   hello world
@@ -7,12 +9,23 @@
 //THIS IS A TEST COMMENT
 
 struct REFLCOMPONENT Character{
-  s8* name;
-  u32 health;
-  u32 atk;
+    s8* name;
+    u32 health;
+    u32 atk;
 };
 
 struct REFLCOMPONENT BossCharacter{
-  Character character;
-  u32 special_ability;
+    Character character;//this is a char name
+    u32 special_ability;/*asdasd*/
 };
+
+struct REFLSTRUCT(GENERIC) Foo{
+    u32 a;
+    u32 b;
+    u32 c;
+};
+
+s32 REFLFUNC(GENERIC) AddFunction(u32 a,u32 b){
+    
+    return a + b;
+}
