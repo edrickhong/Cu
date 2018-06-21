bfab9c7a9d900c5799eb4593ecacf89d81652aa4
edrickhong <edrickhong@yahoo.com.sg>
2018-06-21 14:03:31 -0700

    we do not need the build_linux.sh file anymore. Gonna keep it around to bake stuff only. TODO: rename it. Keeping build_win32 around cos it's the only way to do dynamic recompile

--- build_win32.bat
+++ build_win32.bat
@@ -18,21 +18,3 @@ echo "COMPILING..."
 cd Make
 cmake ..
 msbuild ALL_BUILD.vcxproj /verbosity:quiet
-
-cd ..
-
-rem assuming we are using cl
-
-rem set OUT_DIR=bin\Debug\
-
-rem cd %OUT_DIR%
-
-rem rem MARK: you need to replace the vulkan include path to your vulkan include directory
-rem set INCLUDE_DIR=/I..\..\src\game\ /I..\..\src\engine\ /I..\..\include\ /I..\..\src\cu_std\cu /I..\..\src\cu_std\win32\ /I..\..\src\cu_std\amd64
-
-rem set FLAGS=/Zi /std:c++14
-
-rem set COMMON_LIBS=kernel32.lib User32.lib Ole32.lib
-
-rem game We'll do this later because this needs a bit of work
-rem cl %FLAGS% %INCLUDE_DIR% /LD ..\..\src\game\game.cpp cu_std.lib xaudio2.lib %COMMON_LIBS%
