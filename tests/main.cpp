2399134043363f8553bed7ea57ebd8b3e8ba5790
ehzx <edrickhong@gmail.com>
2019-03-14 17:57:43 +0800

    fixing tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -3,97 +3,6 @@
 #include "ttype.h"
 #include "../src/importer/main.h"
 #include "mdf_models.h"
-#include "pparse.h"
-
-#include "hash_array.h"
-
-struct HashEntry{
-    u64 hash;
-    const s8* string_array[1024 * 8];
-    u32 string_count = 0;
-};
-
-void AddHash(HashEntry* table,u32* c,u64 hash,const s8* string){
-    
-    auto count = *c;
-    
-    
-    
-    for(u32 i = 0; i < count; i++){
-        
-        auto k = &table[i];
-        
-        if(k->hash == hash){
-            
-            k->string_array[k->string_count] = string;
-            k->string_count++;
-            
-            return;
-        }
-        
-    }
-    
-    table[count].hash = hash;
-    table[count].string_array[0] = string;
-    table[count].string_count = 1;
-    count++;
-    
-    *c = count;
-    
-}
-
-logic PrintHashDiagnostics(HashEntry* table,u32 count){
-    
-    u32 collide_count = 0;
-    
-    for(u32 i = 0; i < count; i++){
-        
-        auto entry = &table[i];
-        
-        if(entry->string_count > 1){
-            
-            collide_count += entry->string_count;
-            
-            printf("HASH COLLISION(%llu): ",entry->hash);
-            
-            for(u32 j = 0; j < entry->string_count; j++){
-                
-                printf("%s ",entry->string_array[j]);
-                
-            }
-            
-            printf("\n");
-            
-        }
-        
-    }
-    
-    auto percent = ((f64)collide_count/(f64)(_arraycount(hash_array))) * 100.0f;
-    
-    
-    printf("%d collisions out of %d hashes. (%f percent collision rate)\n",collide_count,(u32)_arraycount(hash_array),percent);
-    
-    return percent > 20.0;
-}
-
-logic HashTest(){
-    
-    printf("RUNNING PHashString Collision test\n");
-    
-    auto hash_table = (HashEntry*)alloc(sizeof(HashEntry) * (_arraycount(hash_array)));
-    
-    u32 hash_count = 0;
-    
-    for(u32 i = 0; i < _arraycount(hash_array); i++){
-        
-        auto hash = PHashString(hash_array[i]);
-        
-        AddHash(hash_table,&hash_count,hash,hash_array[i]);
-    }
-    
-    return PrintHashDiagnostics(hash_table,hash_count);
-    
-}
 
 struct MDFEntry{
     const s8* file;
@@ -165,9 +74,7 @@ u32 MDFTests(){
 
 int main(s32 argc,s8** argv){
     
-    auto res = HashTest();
-    
-    res |= MDFTests();
+    auto res = MDFTests();
     
     return res;
 }
