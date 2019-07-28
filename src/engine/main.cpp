62d936da110163a41012fdf82135f0c1fff1edb6
ehzx <edrickhong@gmail.com>
2019-07-28 17:25:30 +0800

    using vim now. this is its indent. FIXME:sw rendering path for x11 is using a diff format backbuffer than the window, causing failure

--- src/engine/main.cpp
+++ src/engine/main.cpp
@@ -7,191 +7,197 @@ _compile_kill(VK_INDEX_TYPE_UINT16 != 0);
 _compile_kill(VK_INDEX_TYPE_UINT32 != 1);
 
 s32 main(s32 argc,s8** argv){
-    
+
+	TestSW();
+
+	return 0;
+
+
+
 	InitAllSystems();
-    
+
 #ifdef DEBUG
-    
+
 #if 0
-    GetExecFileAssetData();
+	GetExecFileAssetData();
 #endif
-    
+
 #endif
-    
-    void(*reload)(GameReloadData*) = 0;
-    
-    
-    while(gdata->running){
-        
-        {
-            
-            pdata->objupdate_count = 0;
-            TSingleEntryUnlock(&gui_draw_is_locked);
-            
-            Clear(&pdata->rendercontext);
-            ClearLightList();
-            
+
+	void(*reload)(GameReloadData*) = 0;
+
+
+	while(gdata->running){
+
+		{
+
+			pdata->objupdate_count = 0;
+			TSingleEntryUnlock(&gui_draw_is_locked);
+
+			Clear(&pdata->rendercontext);
+			ClearLightList();
+
 #if _enable_gui
-            
-            s8 ascii_buffer[256] = {};
-            u32 ascii_count = 0;
-            
-            if(GUIIsAnyElementActive()){
-                
-                for(u32 i = 0; i < _arraycount(pdata->keyboardstate.curkeystate); i++){
-                    
-                    if(IsKeyPressed(&pdata->keyboardstate,i)){
-                        
-                        auto c = WKeyCodeToASCII(i);
-                        
-                        if(PIsVisibleChar(c)){
-                            ascii_buffer[ascii_count] = c;
-                            ascii_count++;
-                        }
-                        
-                    }
-                    
-                }
-                
-            }
-            
-            GUIUpdate(&pdata->swapchain,&pdata->keyboardstate,ascii_buffer,ascii_count,&pdata->mousestate,
-                      pdata->view,pdata->proj);
-            
-            GUIBegin();
-            
+
+			s8 ascii_buffer[256] = {};
+			u32 ascii_count = 0;
+
+			if(GUIIsAnyElementActive()){
+
+				for(u32 i = 0; i < _arraycount(pdata->keyboardstate.curkeystate); i++){
+
+					if(IsKeyPressed(&pdata->keyboardstate,i)){
+
+						auto c = WKeyCodeToASCII(i);
+
+						if(PIsVisibleChar(c)){
+							ascii_buffer[ascii_count] = c;
+							ascii_count++;
+						}
+
+					}
+
+				}
+
+			}
+
+			GUIUpdate(&pdata->swapchain,&pdata->keyboardstate,ascii_buffer,ascii_count,&pdata->mousestate,
+					pdata->view,pdata->proj);
+
+			GUIBegin();
+
 #endif
-            
-            BUILDGUIGRAPH(gdata->draw_profiler);
-            
-            
-            
-            TimeSpec start,end;
-            
-            MASTERTIMEBLOCKSTART(Aqua);
-            
-            GetTime(&start);
-            
-            {
-                
-                EXECTIMEBLOCK(Black);
-                
-                
-                //MARK: we should move this to buildrendercmdbuffer
-                //FIXME: turning on vsync has frame hitches
-                {
-                    
-                    TIMEBLOCKTAGGED("AcquireImage",Orange);
-                    vkAcquireNextImageKHR(pdata->vdevice.device,
-                                          pdata->swapchain.swap,0xFFFFFFFFFFFFFFFF,
-                                          pdata->waitacquireimage_semaphore,
-                                          0,(u32*)&pdata->swapchain.image_index);  
-                    
-                }
-                
-                
-                UpdateAllocatorTimeStamp();
-                Clear(&pdata->threadqueue);
-                
-                ProcessEvents(&pdata->window,&pdata->keyboardstate,&pdata->mousestate,
-                              &gdata->running);
-                
-                {
-                    TIMEBLOCKTAGGED("RELOADLIB",Firebrick);
-                    pdata->lib = ReloadGameLibrary(pdata->lib,(void**)&reload,&pdata->scenecontext);
-                }
-                
-                
-                if(reload){
-                    
+
+			BUILDGUIGRAPH(gdata->draw_profiler);
+
+
+
+			TimeSpec start,end;
+
+			MASTERTIMEBLOCKSTART(Aqua);
+
+			GetTime(&start);
+
+			{
+
+				EXECTIMEBLOCK(Black);
+
+
+				//MARK: we should move this to buildrendercmdbuffer
+				//FIXME: turning on vsync has frame hitches
+				{
+
+					TIMEBLOCKTAGGED("AcquireImage",Orange);
+					vkAcquireNextImageKHR(pdata->vdevice.device,
+							pdata->swapchain.swap,0xFFFFFFFFFFFFFFFF,
+							pdata->waitacquireimage_semaphore,
+							0,(u32*)&pdata->swapchain.image_index);  
+
+				}
+
+
+				UpdateAllocatorTimeStamp();
+				Clear(&pdata->threadqueue);
+
+				ProcessEvents(&pdata->window,&pdata->keyboardstate,&pdata->mousestate,
+						&gdata->running);
+
+				{
+					TIMEBLOCKTAGGED("RELOADLIB",Firebrick);
+					pdata->lib = ReloadGameLibrary(pdata->lib,(void**)&reload,&pdata->scenecontext);
+				}
+
+
+				if(reload){
+
 					//FIXME(WIN32): we are reloading every frame. seems like a problem with FFileChanged
-                    
-                    auto context = &pdata->scenecontext;
-                    
-                    GameReloadData reloaddata = {
-                        gdata,pdata->vdevice,pdata->renderpass,&pdata->window,context,GetGUIContext(),
-                        GetAAllocatorContext(),DEBUGTIMERGETCONTEXT()
-                    };
-                    
-                    vkDeviceWaitIdle(pdata->vdevice.device);
-                    
-                    reload(&reloaddata);
-                    reload = 0;
-                    
-                    CompileAllPipelines(pdata);
-                }
-                
-                u32 audio_count = 0;
-                EntityAudioData* audio_data;
-                
-                
-                if(pdata->lib.updaterender){
-                    
-                    auto context = &pdata->scenecontext;
-                    context->prev_frametime = pdata->deltatime;
-                    context->keyboardstate = &pdata->keyboardstate;
-                    context->mousestate = &pdata->mousestate;
-                    context->audiocontext = &audio_data;
-                    context->audiocontext_count = &audio_count;
-                    
-                    auto ptr =
-                        (void (*)(SceneContext*))pdata->lib.updaterender;
-                    
-                    ptr(context);
-                }
-                
-                auto frames = AAudioDeviceWriteAvailable(&pdata->audio);
-                
-                if(frames >= pdata->submit_audiobuffer.size_frames){
-                    
-                    auto args = TAlloc(MixAudioLayout,1);
-                    
-                    *args = {&pdata->submit_audiobuffer,audio_data,audio_count,pdata->resample_scale};
-                    
-                    PushThreadWorkQueue(&pdata->threadqueue,
-                                        MixAudio,(void*)args,pdata->worker_sem);
-                    
-                }
-                
-                ProcessDrawList();
-                
-                MainThreadDoWorkQueue(&pdata->threadqueue,0);
-                
-                
+
+					auto context = &pdata->scenecontext;
+
+					GameReloadData reloaddata = {
+						gdata,pdata->vdevice,pdata->renderpass,&pdata->window,context,GetGUIContext(),
+						GetAAllocatorContext(),DEBUGTIMERGETCONTEXT()
+					};
+
+					vkDeviceWaitIdle(pdata->vdevice.device);
+
+					reload(&reloaddata);
+					reload = 0;
+
+					CompileAllPipelines(pdata);
+				}
+
+				u32 audio_count = 0;
+				EntityAudioData* audio_data;
+
+
+				if(pdata->lib.updaterender){
+
+					auto context = &pdata->scenecontext;
+					context->prev_frametime = pdata->deltatime;
+					context->keyboardstate = &pdata->keyboardstate;
+					context->mousestate = &pdata->mousestate;
+					context->audiocontext = &audio_data;
+					context->audiocontext_count = &audio_count;
+
+					auto ptr =
+						(void (*)(SceneContext*))pdata->lib.updaterender;
+
+					ptr(context);
+				}
+
+				auto frames = AAudioDeviceWriteAvailable(&pdata->audio);
+
+				if(frames >= pdata->submit_audiobuffer.size_frames){
+
+					auto args = TAlloc(MixAudioLayout,1);
+
+					*args = {&pdata->submit_audiobuffer,audio_data,audio_count,pdata->resample_scale};
+
+					PushThreadWorkQueue(&pdata->threadqueue,
+							MixAudio,(void*)args,pdata->worker_sem);
+
+				}
+
+				ProcessDrawList();
+
+				MainThreadDoWorkQueue(&pdata->threadqueue,0);
+
+
 #if _enable_gui
-                
-                GUIEnd();
-                
+
+				GUIEnd();
+
 #endif
-                //we can thread this
-                BuildRenderCommandBuffer(pdata);
-                
-                PresentBuffer(pdata);
-            }
-            
-            GetTime(&end);
-            
-            auto diff = GetTimeDifferenceMS(start,end);
-            f32 sleeptime = (_targetframerate - diff);
-            
-            
-            if(sleeptime > 0 ){
-                TIMEBLOCKTAGGED("Sleep",DarkGray);
-                SleepMS(sleeptime);
-            }
-            
-            GetTime(&end);
-            
-            pdata->deltatime = GetTimeDifferenceMS(start,end);
-            
-        }
-        
-    }
-    
-    WritePipelineCache();
-    
-    return 0;
-    
+				//we can thread this
+				BuildRenderCommandBuffer(pdata);
+
+				PresentBuffer(pdata);
+			}
+
+			GetTime(&end);
+
+			auto diff = GetTimeDifferenceMS(start,end);
+			f32 sleeptime = (_targetframerate - diff);
+
+
+			if(sleeptime > 0 ){
+				TIMEBLOCKTAGGED("Sleep",DarkGray);
+				SleepMS(sleeptime);
+			}
+
+			GetTime(&end);
+
+			pdata->deltatime = GetTimeDifferenceMS(start,end);
+
+		}
+
+	}
+
+	WritePipelineCache();
+
+	return 0;
+
 }
 
 //add cpp's here to limit scope
@@ -208,43 +214,43 @@ s32 main(s32 argc,s8** argv){
 
 /*
 
-**VK_KHR_16bit_storage                : extension revision  1
+ **VK_KHR_16bit_storage                : extension revision  1
  **VK_KHR_8bit_storage                 : extension revision  1
  **VK_KHR_driver_properties            : extension revision  1
  **VK_KHR_relaxed_block_layout         : extension revision  1
  **VK_KHR_uniform_buffer_standard_layout: extension revision  1
  **VK_KHR_variable_pointers            : extension revision  1
  **VK_KHR_vulkan_memory_model          : extension revision  3
- 
-libpapi-dev
-libdbus-1-dev
-wayland-protocols
-libvulkan-dev
-vulkan-validationlayers
-zenity
+
+ libpapi-dev
+ libdbus-1-dev
+ wayland-protocols
+ libvulkan-dev
+ vulkan-validationlayers
+ zenity
 
 TODO: 
 
-  Make keyboardstate and mousestate a bit array
-  
-  Compile all assets into an adb file (asset data base). We will build a function a constexpr
-  function at compile time which translates filepaths to indices and an adb file w raw data.
-  We will keep the adb file open at all times and just read from the offset as required.
-  
-  Beef up our profiler
-  We can profile the cmdbuffers using this. We should inject and record this too
-  
-  vkCmdWriteTimestamp();
-  
-  
-  memory protect our allocations
-  Support drawing multiple objects off a single buffer(use vert & index buffer offsets into buffer)
-  implement quaternion double cover
-  Implement Dual quaternion blending in MDF
-  
-  multi gpu functions
-  
-  void vkCmdSetDeviceMask(
+Make keyboardstate and mousestate a bit array
+
+Compile all assets into an adb file (asset data base). We will build a function a constexpr
+function at compile time which translates filepaths to indices and an adb file w raw data.
+We will keep the adb file open at all times and just read from the offset as required.
+
+Beef up our profiler
+We can profile the cmdbuffers using this. We should inject and record this too
+
+vkCmdWriteTimestamp();
+
+
+memory protect our allocations
+Support drawing multiple objects off a single buffer(use vert & index buffer offsets into buffer)
+implement quaternion double cover
+Implement Dual quaternion blending in MDF
+
+multi gpu functions
+
+void vkCmdSetDeviceMask(
 VkCommandBuffer commandBuffer, uint32_t deviceMask);
 
 VkResult vkAcquireNextImage2KHR(
@@ -278,26 +284,26 @@ REMOTE,
 SUM,
 LOCAL_MULTI_DEVICE
 typedef struct VkDeviceGroupRenderPassBeginInfo {
-VkStructureType sType; const void* pNext;
-uint32_t deviceMask;
-uint32_t deviceRenderAreaCount;
-const VkRect2D* pDeviceRenderAreas; P.15
+	VkStructureType sType; const void* pNext;
+	uint32_t deviceMask;
+	uint32_t deviceRenderAreaCount;
+	const VkRect2D* pDeviceRenderAreas; P.15
 } VkDeviceGroupRenderPassBeginInfo;
 typedef struct VkDeviceGroupSubmitInfo {
-VkStructureType sType; const void* pNext;
-uint32_t waitSemaphoreCount;
-const uint32_t* pWaitSemaphoreDeviceIndices;
-uint32_t commandBufferCount;
-const uint32_t* pCommandBufferDeviceMasks;
-uint32_t signalSemaphoreCount;
-const uint32_t* pSignalSemaphoreDeviceIndices;
+	VkStructureType sType; const void* pNext;
+	uint32_t waitSemaphoreCount;
+	const uint32_t* pWaitSemaphoreDeviceIndices;
+	uint32_t commandBufferCount;
+	const uint32_t* pCommandBufferDeviceMasks;
+	uint32_t signalSemaphoreCount;
+	const uint32_t* pSignalSemaphoreDeviceIndices;
 } VkDeviceGroupSubmitInfo;
 
 
 typedef struct VkMemoryAllocateFlagsInfo {
-VkStructureType sType; const void* pNext;
-VkMemoryAllocateFlags flags;
-uint32_t deviceMask;
+	VkStructureType sType; const void* pNext;
+	VkMemoryAllocateFlags flags;
+	uint32_t deviceMask;
 } VkMemoryAllocateFlagsInfo;
 
 
